897e4e995fb9d72953500ec4d57b838c6e86b9da
Rogerio A Serapiao <rserapa@gmail.com>
2021-05-29 17:03:06 -0300

    add create, update and delete tags in Contents

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,16 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+// import from construct Tags injection to Contents
+import $ from 'jquery'
+import 'select2'
+
+$(document).on('turbolinks:load', function(){
+    $(".select2").select2({
+        tags: true,
+        tokenSeparators: [',', ' ']
+    })
+});
+
