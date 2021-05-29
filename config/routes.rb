69fdbb734074bcd37df327683278d338a5dc9b16
Rogerio A Serapiao <rserapa@gmail.com>
2021-05-28 21:23:27 -0300

    add form new contents https://trello.com/c/iUDO0vOz/8-criar-o-crud-de-conte%C3%BAdo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'home/index'
+  get 'home', to: 'home#index'
   
   #get 'contents', to: 'contents#index'
   #get 'contents/new', to: 'contents#new'
