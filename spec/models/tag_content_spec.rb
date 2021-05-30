0357c5c492006e338245123d2177edd8dedd4212
Rogerio A Serapiao <rserapa@gmail.com>
2021-05-30 16:40:21 -0300

    Test examples model Tags
    work: https://trello.com/c/PGu8vdgC/20-exemplos-de-teste

--- spec/models/tag_content_spec.rb
+++ spec/models/tag_content_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TagContent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+RSpec.describe Tag, type: :model do
+  it { should have_many(:contents) } 
+end  
+
